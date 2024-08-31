b5c9333ac266f63765de287fef6fa4e725451a46
Tanushkarthi <tanushkarthikeyan2022@gmail.com>
2024-08-31 19:16:26 +0530

    Completed Full Portfolio

---
+++ src/data/constants.js
@@ -0,0 +1,267 @@
+export const Bio = {
+  name: "Tanush Karthikeyan",
+  roles: [
+    "Full Stack Developer",
+    "Flutter Developer",
+    "Python Developer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/Tanushkarthi",
+  resume:
+    "https://drive.google.com/file/d/1Dyk6qbXfzjaocluUXiQnaHh1FGk7hh2T/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/tanush-karthikeyan/",
+  twitter: "https://x.com/TanushKarthi",
+  insta: "https://www.instagram.com/tanushkarthikeyan2003/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "Flutter",
+        image:
+          "https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png",
+      },
+      {
+        name: "Figma",
+        image: "https://upload.wikimedia.org/wikipedia/commons/3/33/Figma-logo.svg"
+      }
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+    ],
+  },
+  {
+    title: "Languages",
+    skills: [
+      {
+        name: "C",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/1/19/C_Logo.png",
+      },
+      {
+        name: "C++",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/459px-ISO_C%2B%2B_Logo.svg.png",
+      },
+      {
+        name: "Java",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkCCdLCDhY7mwKXP4bHVQ1SFZxn3pPM2QLFw&s",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://static-00.iconduck.com/assets.00/git-icon-2048x2048-juzdf1l5.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://seeklogo.com/images/P/postman-logo-0087CA0D15-seeklogo.com.png",
+      },
+      {
+        name: "Android Studio",
+        image:
+          "https://seeklogo.com/images/A/android-studio-2023-logo-0DAB29430B-seeklogo.com.png"
+      }
+    ],
+  },
+
+];
+
+
+export const education = [
+  {
+    id: 0,
+    img: "https://media.licdn.com/dms/image/v2/C4E0BAQGFXFoas-bS0A/company-logo_200_200/company-logo_200_200/0/1630574614608/wekceians_logo?e=2147483647&v=beta&t=TVfzy4TlRYiG3VeJWDMciwQ4L3P9V2wZmvRr6-laMFE",
+    school: "Karpagam College of Engineering, Coimbatore",
+    date: "Oct 2020 - May 2024",
+    grade: "8.41 CGPA",
+    desc: "I Completed Bachelor's degree in Computer Science and Engineering at Karpagam College of Engineering, Coimbatore. I have completed 4 years and have a CGPA of 8.41. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+    degree: "Bachelor of Engineering - BE, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYTMmuiaJzB7sSM5XQqcSOES_LQ1m2VXYX3A&s",
+    school: "Swamy Vivekananda Matriculation Higher Secondary School, Salem",
+    date: "June 2019 - Apr 2020",
+    grade: "60%",
+    desc: "I completed my class 12 high school education at Swamy Vivekananda Matriculation Higher Secondary School, Salem, where I studied Science with Computer Science.",
+    degree: "HSC(XII), Science with Computer",
+  },
+  {
+    id: 2,
+    img: "https://nuscma.com/photo/2023-09-19relaince1.jpeg",
+    school: "Reliance Matriculation Higher Secondary School, Idappadi",
+    date: "June 2017 - Apr 2018",
+    grade: "67%",
+    desc: "I completed my class 10 education at Reliance Matriculation Higher Secondary School, Idappadi, where I studied Science with Computer Science.",
+    degree: "SSLC(X), Science",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "TK-Mart",
+    date: "Aug 2020 - Oct 2021",
+    description:
+      "I developed the front-end for TK-Mart e-commerce app using Flutter, focusing on user experience, feedback incorporation, and best practices in front-end development. The project showcased my skills in managing complex tasks and delivering quality results timely.",
+    image:
+      "https://img.freepik.com/free-vector/fashion-shopping-app-collection-template_23-2148654438.jpg?w=996&t=st=1724689962~exp=1724690562~hmac=a2504f74094ba1b4202a7864436752d33dd2ebfa0be1450eb7c83f37023c116e",
+    tags: [
+      "Flutter",
+      "Firbase",
+    ],
+    category: "android app",
+    github: "https://github.com/Tanushkarthi/TK_Mart.git",
+  },
+  {
+    id: 0,
+    title: "Detecting Offensive Language in Youtube Comments and Audios",
+    date: "Dec 2023 - July 2024",
+    description:
+      "Implemented machine learning algorithms like mBERT, BERT Base, BERT Large, DistilBERT, and RoBERTa to detect offensive language in YouTube comments and audios, showcasing expertise in natural language processing, model training, and algorithm application.",
+    image:
+      "https://user-images.githubusercontent.com/64485885/234602896-a1bd8bcc-b72b-4821-83d6-8ad885bf435e.png",
+    tags: ["Python", "Node Js", 'Machine Learning'],
+    category: "machine learning",
+    github: "https://github.com/Tanushkarthi/Final_Year_Project.git",
+    member: [
+      {
+        name: "Jishnu",
+        img: "https://avatars.githubusercontent.com/u/69377713?v=4",
+        linkedin: "https://www.linkedin.com/in/jishnubk/",
+        github: "https://github.com/Jishnu-26",
+      },
+      {
+        name: "Tanush",
+        img: "https://avatars.githubusercontent.com/u/169248343?v=4",
+        linkedin: "https://www.linkedin.com/in/tanush-karthikeyan/",
+        github: "https://github.com/Tanushkarthi",
+      },
+      {
+        name: "Udhayarajan",
+        img: "https://avatars.githubusercontent.com/u/77388817?v=4",
+        linkedin: "https://www.linkedin.com/in/udhayarajan-m/",
+        github: "https://github.com/Udhayarajan",
+      },
+    ],
+  },
+  {
+    id: 1,
+    title: "Fusion X",
+    date: "Oct 2024 - Present",
+    description:
+      "Worked on the Fusion-X project, contributing to front-end development and database management using React and Next.js. Demonstrated teamwork, multitasking, and skills in delivering a high-quality project.",
+    image:
+      "https://user-images.githubusercontent.com/64485885/234916413-96296f13-fe4b-4cc4-b215-e72bd7c27928.png",
+    tags: [
+      "React Js",
+      "Next Js",
+    ],
+    category: "web app",
+    github: "https://github.com/dFusionX/api-hub-fe.git",
+    member: [
+      {
+        name: "Jishnu",
+        img: "https://avatars.githubusercontent.com/u/69377713?v=4",
+        linkedin: "https://www.linkedin.com/in/jishnubk/",
+        github: "https://github.com/Jishnu-26",
+      },
+      {
+        name: "Tanush",
+        img: "https://avatars.githubusercontent.com/u/169248343?v=4",
+        linkedin: "https://www.linkedin.com/in/tanush-karthikeyan/",
+        github: "https://github.com/Tanushkarthi",
+      },
+      {
+        name: "Udhayarajan",
+        img: "https://avatars.githubusercontent.com/u/77388817?v=4",
+        linkedin: "https://www.linkedin.com/in/udhayarajan-m/",
+        github: "https://github.com/Udhayarajan",
+      },
+      {
+        name: "Srihari",
+        img: "https://avatars.githubusercontent.com/u/112186853?v=4",
+        linkedin: "https://www.linkedin.com/in/vb-srihari/",
+        github: "https://github.com/SRIHARI-VB"
+      }
+    ],
+  },
+
+];
